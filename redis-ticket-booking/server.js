6d3b36b87b59fc553f94c1609d0a1ce7a9531ca7
sahilpunia04 <sahilpreet0407@gmail.com>
2026-02-26 11:57:38 +0530

    Restructured project and updated seat system

--- redis-ticket-booking/server.js
+++ redis-ticket-booking/server.js
@@ -3,7 +3,9 @@ const app = require("./src/app");
 const { connectRedis } = require("./src/config/redis");
 
 const PORT = process.env.PORT || 5000;
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 async function startServer() {
   try {
     await connectRedis(); // connect redis first
